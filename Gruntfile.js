8f0438721260b2e8e897dccd15b5bd1c00da59ad
Alina <alina.mihai90@gmail.com>
2015-12-14 17:33:43 +0200

    minor fixes

--- Gruntfile.js
+++ Gruntfile.js
@@ -356,6 +356,7 @@ module.exports = function (grunt) {
             'bower_components/**/*',
             'assets/images/{,*/}*.{webp}',
             'assets/fonts/**/*',
+            'assets/angulargrid-master/*',
             'index.html'
           ]
         }, {
