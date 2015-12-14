b120c70e5b629565ef219aa9604295641c94f1b1
Alina <alina.mihai90@gmail.com>
2015-12-14 09:46:59 +0200

    create PinService as a core component on the front end side and implement the addPin method

---
+++ client/components/core/pin.service.js
@@ -0,0 +1,32 @@
+(function() {
+    'use strict';
+
+    angular
+        .module('pInterestCloneApp')
+        .service('PinService', PinService);
+
+    PinService.$inject = ['$http','$q'];
+
+    /* @ngInject */
+    function PinService($http,$q) {
+    	this.pins=[];
+        this.addPin = addPin;
+
+        ////////////////
+
+        function addPin(pin) {
+        	var deferred=$q.defer();
+        	var svc=this;
+        	$http.post('/api/pins',pin)
+        	.success(function(pin){
+ 				svc.pins.push(pin);
+        		deferred.resolve(pin);
+        	});
+        	.error(function(err){
+        		deferred.reject(err);
+        	});
+
+        	return deferred.promise;
+        }
+    }
+})();
